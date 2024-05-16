b14f522619f56bfb54dd593e0d7fc78c45269028
Matija M <margeta.matija@gmail.com>
2024-05-16 19:55:07 +0200

    Fix date format

--- Azil.DAL/DataModel/DnevnikUdomljavanja.cs
+++ Azil.DAL/DataModel/DnevnikUdomljavanja.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Azil.DAL.DataModel
 {
@@ -8,7 +10,25 @@ namespace Azil.DAL.DataModel
     {
         public int id_ljubimca { get; set; }
         public int id_korisnika { get; set; }
+
+        [JsonPropertyName("datum")]
+        [JsonConverter(typeof(DateFormatConverter2))]
         public DateTime datum { get; set; }
         public string opis { get; set; }
+    }
+
+    public class DateFormatConverter2 : JsonConverter<DateTime>
+    {
+        private readonly string _dateFormat2 = "yyyy-MM-dd";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(), _dateFormat2, null);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_dateFormat2));
+        }
     }
 }
--- Azil.DAL/DataModel/KorisnikUloga.cs
+++ Azil.DAL/DataModel/KorisnikUloga.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 
 namespace Azil.DAL.DataModel
 {
@@ -8,7 +8,28 @@ namespace Azil.DAL.DataModel
     {
         public int id_korisnika { get; set; }
         public int id_uloge { get; set; }
+
+        [JsonPropertyName("datum_od")]
+        [JsonConverter(typeof(DateFormatConverter))]
         public DateTime datum_od { get; set; }
+
+        [JsonPropertyName("datum_do")]
+        [JsonConverter(typeof(DateFormatConverter))]
         public DateTime datum_do { get; set; }
+    }
+
+    public class DateFormatConverter : JsonConverter<DateTime>
+    {
+        private readonly string _dateFormat = "yyyy-MM-dd";
+
+        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            return DateTime.ParseExact(reader.GetString(), _dateFormat, null);
+        }
+
+        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString(_dateFormat));
+        }
     }
 }
